7b303839f7e02141a97872b7153b33db04d9d6e7
SolarionsMH <trab.manuelh@gmail.com>
2024-06-08 20:21:44 -0500

    Conexión con backend agregada

--- frontend/wallet-app/vite.config.js
+++ frontend/wallet-app/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+// Actualización de conexión frontend-backend
+
+import { defineConfig } from "vite"
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': 'https://127.0.0.1:5000'
+    }
+  }
 })
